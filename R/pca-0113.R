1cdcf5084d13a008fd4d7f0ab024c40e93b741f5
ronxin <ronxin@umich.edu>
2015-01-13 16:52:16 -0500

    Added R script to process multiple PCA

---
+++ R/pca-0113.R
@@ -0,0 +1,20 @@
+input_prefix <- '~/word2vec/output/10w-vectors/vector-wiki-10word-20150113-csv-'
+output_prefix <- '~/word2vec/output/10w-pcas/wiki-10word-20150113-'
+
+normalize_vectors <- function(d) {
+  for (i in 1:nrow(dat)) {
+    dat[i,] <- dat[i,] / sqrt(sum(dat[i,] * dat[i,]))
+  }	
+}
+
+for (i in 0:10) {
+  filename <- paste(input_prefix, i, sep='')
+  dat <- read.table(filename, sep="\t", row.names=1)
+  normalize_vectors(dat)
+  pca <- prcomp(dat)
+  pngname <- paste(output_prefix, i, '.png', sep='')
+  png(pngname)
+  plot(pca$x, xlim=c(min(pca$x[,1]-.2), max(pca$x[,1]+.2)), ylim=c(min(pca$x[,2]-.2), max(pca$x[,2]+.2)))  # print the first two PCA dimensions
+  text(pca$x[,1], pca$x[,2], rownames(dat), pos=3)  # add text labels
+  dev.off()
+}
